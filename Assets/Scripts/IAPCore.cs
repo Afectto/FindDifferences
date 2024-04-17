0abad824e3a948964562b1699885d8f6ce902086
Afectto <138150311+Afectto@users.noreply.github.com>
2024-04-17 20:12:21 +0700

    IAP

---
+++ Assets/Scripts/IAPCore.cs
@@ -0,0 +1,132 @@
+using System;
+using UnityEngine;
+using UnityEngine.Purchasing;
+
+public class IAPCore : MonoBehaviour, IStoreListener
+{
+    private static IStoreController m_StoreController;
+    private static IExtensionProvider m_StoreExtensionProvider; 
+
+    public static string AddSeconds = "AddSeconds";
+
+    public static Action<int> OnAddTimerLimit;
+    void Start()
+    {
+        if (m_StoreController == null)
+        {
+            InitializePurchasing();
+        }
+    }
+
+    public void InitializePurchasing()
+    {
+        if (IsInitialized())
+        {
+            return;
+        }
+
+        var builder = ConfigurationBuilder.Instance(StandardPurchasingModule.Instance());
+
+        builder.AddProduct(AddSeconds, ProductType.NonConsumable);
+
+        UnityPurchasing.Initialize(this, builder);
+    }
+
+    public void Buy_AddSeconds()
+    {
+        BuyProductID(AddSeconds);
+    }
+
+    void BuyProductID(string productId)
+    {
+        if (IsInitialized())
+        {
+            Product product = m_StoreController.products.WithID(productId);
+
+            if (product != null && product.availableToPurchase)
+            {
+                Debug.Log(string.Format("Purchasing product asychronously: '{0}'", product.definition.id));
+                m_StoreController.InitiatePurchase(product);
+            }
+            else
+            {
+                Debug.Log("BuyProductID: FAIL. Not purchasing product, either is not found or is not available for purchase");
+            }
+        }
+        else
+        {
+            Debug.Log("BuyProductID FAIL. Not initialized.");
+        }
+    }
+
+    public void OnInitializeFailed(InitializationFailureReason error, string message)
+    {
+        throw new NotImplementedException();
+    }
+
+    public PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs args)
+    {
+        if (String.Equals(args.purchasedProduct.definition.id, AddSeconds, StringComparison.Ordinal))
+        {
+            OnAddTimerLimit?.Invoke(10);
+        }
+
+        return PurchaseProcessingResult.Complete;
+    }
+
+    public void RestorePurchases()
+    {
+        if (!IsInitialized())
+        {
+            Debug.Log("RestorePurchases FAIL. Not initialized.");
+            return;
+        }
+
+        if (Application.platform == RuntimePlatform.IPhonePlayer ||
+            Application.platform == RuntimePlatform.OSXPlayer)
+        {
+            Debug.Log("RestorePurchases started ...");
+
+            var apple = m_StoreExtensionProvider.GetExtension<IAppleExtensions>();
+
+            apple.RestoreTransactions((result) =>
+            {
+                Debug.Log("RestorePurchases continuing: " + result + ". If no further messages, no purchases available to restore.");
+            });
+        }
+        else
+        {
+            Debug.Log("RestorePurchases FAIL. Not supported on this platform. Current = " + Application.platform);
+        }
+    }
+
+    private void Awake()
+    {
+
+    }
+
+    public void OnInitialized(IStoreController controller, IExtensionProvider extensions)
+    {
+        Debug.Log("OnInitialized: PASS");
+        m_StoreController = controller;
+        m_StoreExtensionProvider = extensions;
+    }
+
+    private bool IsInitialized()
+    {
+        return m_StoreController != null && m_StoreExtensionProvider != null;
+    }
+
+    public void OnInitializeFailed(InitializationFailureReason error)
+    {
+        Debug.Log("OnInitializeFailed InitializationFailureReason:" + error);
+    }
+
+    public void OnPurchaseFailed(Product product, PurchaseFailureReason failureReason)
+    {
+        Debug.Log(string.Format("OnPurchaseFailed: FAIL. Product: '{0}', PurchaseFailureReason: {1}", product.definition.storeSpecificId, failureReason));
+    }
+
+
+
+}
--- Assets/Scripts/Time/TimeMonoBehaviour.cs
+++ Assets/Scripts/Time/TimeMonoBehaviour.cs
@@ -8,23 +8,31 @@ public class TimerMonoBehaviour : MonoBehaviour
     
     private Timer _timer;
     
-    private const int TIMER_LIMIT = 2;
+    private const int TIMER_MIN_LIMIT = 2;
+    private int _currentLevelTimerLimit;
     
     public static Action OnTimerIsOver;
 
     void Start()
     {
         _timer = new Timer();
+        _currentLevelTimerLimit = TIMER_MIN_LIMIT * 60;
         OnStartTimer();
         CounterDifferences.OnAllDifferencesFind += OnAllDifferencesFind;
         AppadealManager.OnRewardedVideoFinishedAction += OnRewardedVideoFinished;
+        IAPCore.OnAddTimerLimit += OnAddTimerLimit;
+    }
+
+    private void OnAddTimerLimit(int value)
+    {
+        _currentLevelTimerLimit += value;
     }
 
     void Update()
     {
         _timer.Update();
         _timerText.text = _timer.GetTimeFormatted();
-        if (_timer.GetMinutes() >= TIMER_LIMIT)
+        if (_timer.GetMinutes()*60 + _timer.GetSeconds() >= _currentLevelTimerLimit)
         {
             _timer.StopTimer();
             OnTimerIsOver.Invoke();
@@ -38,6 +46,7 @@ public class TimerMonoBehaviour : MonoBehaviour
 
     private void OnStartTimer()
     {
+        _currentLevelTimerLimit = TIMER_MIN_LIMIT * 60;
         _timer.Reset();
         _timer.Start();
     }
